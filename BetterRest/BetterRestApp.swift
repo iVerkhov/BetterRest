95a7b4c449ad270bae16142008ecae8288e1e28c
iVerkhov <vrkhv@iCloud.com>
2023-08-06 16:16:36 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Игорь Верхов on 06.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
